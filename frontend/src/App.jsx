d31ec91b0ac331dec9240de2dab54edf6448c4c9
Mevini Silva <silvamevini19@gmail.com>
2025-11-13 21:46:28 +0530

    adding home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+// src/App.js
+import React from 'react';
+import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
-import './App.css'
+// Create a theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1a237e',
+    },
+    secondary: {
+      main: '#ffd600',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
 
 function App() {
- 
-
   return (
-    <>
-    <h1>Vehicle Valuation and Analytics Management System</h1>
-     
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/assets/assets.jsx
@@ -0,0 +1,8 @@
+// src/assets/index.js
+import Frame12 from "./img/Frame12.png";
+import Service1 from "./img/Service1.png";
+import Service2 from "./img/Service2.png";
+import Service3 from "./img/Service3.png";
+import Car from "./img/Frame15.png";
+
+export { Frame12, Service1, Service2, Service3, Car };
