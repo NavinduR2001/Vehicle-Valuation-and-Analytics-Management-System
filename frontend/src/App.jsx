5abddfeeb5870c7eb96873cf544f6a4ef03f4a09
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-12-02 23:52:30 +0530

    Update pages and main file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,19 @@
-
-// src/App.js
 import React from 'react';
 import './App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
+import Home from './pages/home/Home';
+import AdminDashboard from './pages/admin-dashboard/AdminDashboard';
 
-// Create a theme
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1a237e',
-    },
-    secondary: {
-      main: '#ffd600',
-    },
-  },
-  typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
   );
 }
 
 export default App;
-
-import React from 'react'
-
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-
-function App() {
- 
-
-  return (
-    <>
-    <Navbar/>
-    
-    <h1>Vehicle Valuation and Analytics Management System</h1>
-
-    <Footer/>
-     
-    </>
-  )
-}
-
-export default App
-
