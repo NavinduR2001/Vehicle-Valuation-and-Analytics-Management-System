483ac8fcacadc608802b9703f932d89d3e10132d
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2026-09-03 10:40:52 +0530

    update hosting issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,14 +14,15 @@ import Register from "./Pages/register/Register";
 import UserDashboard from "./Pages/user-dashboard/UserDashboard";
 
 // Manager dashboard
-import ManagerDashboard from "./Pages/manager/ManagerDashboard";
-import ManagerInspect from "./Pages/manager/ManagerInspect";
+import ManagerDashboard from "./Pages/Manager/ManagerDashboard";
+import ManagerInspect from "./Pages/Manager/ManagerInspect";
 
 // Admin dashboard
-import NewAdminDashboard from './Pages/admin/NewAdminDashboard';
+import NewAdminDashboard from "./pages/admin/NewAdminDashboard";
 import AdminInspect from './Pages/admin/AdminInspect';
 
 import "./App.css";
+
 
 const theme = createTheme({
   components: {
